bc563160cb6102c5bc8d62b6004810e339b8579e
szaqal <malczyk.pawel@gmail.com>
2017-04-12 15:07:51 +0200

    3 workers

--- docker/img/start-slave.sh
+++ docker/img/start-slave.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbin/start-slave.sh -c 1 -m 1G -p 7078 spark://spark-master:7077 && tail -f /spark/logs/*
+sbin/start-slave.sh -c 1 -m 512m -p ${SLAVE_PORT} spark://spark-master:7077 && tail -f /spark/logs/*
