184fd787a916fd5559d76daf8670414d937fbbc4
szaqal <malczyk.pawel@gmail.com>
2017-04-13 17:05:17 +0200

    Registration Spark on mesos

---
+++ mesos_register.sh
@@ -0,0 +1,1 @@
+./start-mesos-dispatcher.sh --master mesos://192.168.2.11:5050  --webui-port 8888 -h 192.168.2.11
--- submit_mesos.sh
+++ submit_mesos.sh
@@ -3,6 +3,7 @@
 /home/malczyk/Devel/tools/spark/bin/spark-submit \
 	--master mesos://192.168.2.11:7077 \
 	--deploy-mode cluster \
+ 	--supervise \
 	--executor-memory 512m \
 	--total-executor-cores 1 \
 	--class example.Hello \
