e123f6dedad0b278e4ae7af1cf47063b12c1a4b9
szaqal <malczyk.pawel@gmail.com>
2017-04-20 08:34:51 +0200

    ML initial test

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -5,7 +5,5 @@ source commons.sh
 $SPARK_HOME/bin/spark-submit \
 	--master spark://192.168.2.11:7077 \
 	--class example.JdbcDemo \
-	--driver-class-path /VMS/repos/spark/mysql-connector-java-5.1.41.jar \
-	--jars /VMS/repos/spark/mysql-connector-java-5.1.41.jar \
 /VMS/repos/spark/spark-demo/target/scala-2.11/hello_2.11-0.1.0-SNAPSHOT.jar
 
